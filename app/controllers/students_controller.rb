d4da16e26c39c71f66ea5f6bdebdcb67bbf23113
Elijah <efloren@siue.edu>
2019-06-22 20:00:06 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,10 +2,21 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+    render 'students/index.html.erb'
   end
 
   def show
     @student = Student.find(params[:id])
+    render 'students/show.html.erb'
   end
 
+  def new
+    render 'students/new.html.erb'
+  end
+
+  def create
+    Student.create(first_name: params["student"]["first_name"], last_name: params["student"]["last_name"])
+    @students = Student.all
+    render 'students/index.html.erb'
+  end
 end
