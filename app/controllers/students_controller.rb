7b3171c104b200c21718770cafef65f683d49127
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-24 07:43:52 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,22 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    #create a new action to render a form
+  end
+
+  def create
+    # new action to create
+    # create a new Student object with the values from params and redirect to index page
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
   end
 
 end
